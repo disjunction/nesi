bda928ea97c5c60715d441badaa3b4ffa2831dd9
or <or@pluseq.com>
2015-10-27 23:25:20 +0100

    fix style in jasmine.js, add use strict

--- test/jasmine.js
+++ test/jasmine.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
+"use strict";
 
-var path = require('path'),
-    Command = require('jasmine/lib/command.js'),
-    Jasmine = require('jasmine/lib/jasmine.js');
+let path = require("path"),
+    Command = require("jasmine/lib/command.js"),
+    Jasmine = require("jasmine/lib/jasmine.js");
 
-var jasmine = new Jasmine({ projectBaseDir: path.resolve() });
-var examplesDir = path.join(__dirname, '..', 'node_modules', 'jasmine-core', 'lib', 'jasmine-core', 'example', 'node_example');
-var command = new Command(path.resolve(), examplesDir, console.log);
+let jasmine = new Jasmine({ projectBaseDir: path.resolve() });
+let examplesDir = path.join(__dirname, "..", "node_modules", "jasmine-core", "lib", "jasmine-core", "example", "node_example");
+let command = new Command(path.resolve(), examplesDir, console.log);
 
 // or: hack1 - remove dot-reporter
 jasmine.configureDefaultReporter({print: () => {} });
 
 // or: hack2 - add custom reporter
-var SpecReporter = require('jasmine-spec-reporter');
-var reporter = new SpecReporter();
+let SpecReporter = require("jasmine-spec-reporter");
+let reporter = new SpecReporter();
 jasmine.addReporter(reporter);
 
 command.run(jasmine, process.argv.slice(2));
